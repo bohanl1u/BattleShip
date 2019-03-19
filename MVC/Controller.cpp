7145cdfd60ba21f251667aa2b2946a7c8fc5d1ab
Jimmy Liu <jimmybliu@gmail.com>
2019-03-19 15:40:35 -0700

    modified controller and main

--- MVC/Controller.cpp
+++ MVC/Controller.cpp
@@ -13,12 +13,20 @@
 #include "CheatingAI.h"
 #include "RandomAI.h"
 #include "HuntDestroyAI.h"
-BattleShip::Controller::Controller() {
+BattleShip::Controller::Controller(): view(std::make_unique<StandardView>(std::cin, std::cout)) {
 
 }
 
 void BattleShip::Controller::setupGame(const std::string &GameConfigurationFile, int seed) {
+// in setupGame, you should make sure to setup the game so that it is ready to begin.
+// So you need at least to:
+//Load game configuration from the file.
+//get the player configuration (type of game ).
+//create and add the players to your game
+// ( you can see in the add player function, it calls placeShips for each player..
+// so this is part of the setup, and set the opponents for all the players
 
+    AiPlayer::seed_random_number_generator(seed);
 }
 
 void BattleShip::Controller::setupGame(const std::string &GameConfigurationFile) {
@@ -30,5 +38,6 @@ void BattleShip::Controller::setupGame() {
 }
 
 void BattleShip::Controller::playGame() {
-
+//in PlayGame: you can get the move from the current player,
+// enact the move then change the turn until the game is over!
 }
--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@
 #include <algorithm>
 
 int main(int argc, char** argv) {
-
+  // Create Controller object
+  BattleShip::Controller controller;
+  controller.setupGame();
+  controller.playGame();
 
   return 0;
 }
