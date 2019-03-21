613e93fb766bb2db74c683864e4e861f9f90ba7b
Jimmy Liu <jimmybliu@gmail.com>
2019-03-20 17:48:35 -0700

    stdview

--- MVC/StandardView.cpp
+++ MVC/StandardView.cpp
@@ -8,67 +8,144 @@
 #include "StandardView.h"
 #include "Utility.h"
 
-BattleShip::StandardView::StandardView() {
-
+BattleShip::StandardView::StandardView(){
 }
 
-BattleShip::StandardView::StandardView(std::istream &in, std::ostream &out) {
 
+BattleShip::StandardView::StandardView(std::istream& in, std::ostream& out): in(in), out(out){
 }
 
-BattleShip::PlayerConfiguration BattleShip::StandardView::getPlayerConfiguration() {
-    return BattleShip::PlayerConfiguration(0, 0);
+BattleShip::PlayerConfiguration BattleShip::StandardView::getPlayerConfiguration(){
+    int numPlayas;
+
+    out<<"What type of game do you want to play?\n"
+         "1. Human vs Human\n"
+         "2. Human vs AI\n"
+         "3. AI vs AI\n"
+         "Your choice: "<<endl;
+
+
+    in>>numPlayas;
+
+    if(numPlayas== 1){
+        BattleShip::PlayerConfiguration playerConfiguration(2,0);
+    }else
+    if(numPlayas== 2){
+        BattleShip::PlayerConfiguration playerConfiguration(1,1);
+    }else
+    if(numPlayas== 3){
+        BattleShip::PlayerConfiguration playerConfiguration(0,2);
+    }
+
+    return playerConfiguration;
 }
+
+int BattleShip::StandardView::getAiChoice(){
+    int numAI;
+
+    out<<"What AI do you want?\n"
+         "1. Cheating AI\n"
+         "2. Random AI\n"
+         "3. Hunt Destroy AI\n"
+         "Your choice:"<<endl;
+
+    in>>numAI;
+
+
+    return numAI;
+}
+
 
 std::string BattleShip::StandardView::getPlayerName(int i) {
-    return std::string();
+    std::cout<<"Player 1 please enter your name:"<<endl;
+    std::string name;
+    verifiedRead(in, name);
+    return name;
 }
 
-ShipPlacement BattleShip::StandardView::getShipPlacement(const BattleShip::Player &player, char shipChar, int shipLen) {
-    return ShipPlacement();
-}
 
-void BattleShip::StandardView::updateShipPlacementView(const BattleShip::Player &player) {
-
-}
-
-std::pair<int, int> BattleShip::StandardView::getFiringCoordinate(const BattleShip::Player &attacker) {
-    return std::pair<int, int>();
-}
-
-void BattleShip::StandardView::showWinner(const BattleShip::Player &winner) {
+void BattleShip::StandardView::printBoardAsObscured(const Board &board) {
+    std::vector<std::string> hidBoard = board.getHiddenVersion();
+    for(int i = 0; i < board.getNumRows();i++){
+        std::cout<<hidBoard[i]<<endl;
+    }
 
 }
 
-void BattleShip::StandardView::showResultOfAttack(const BattleShip::Player &attacker,
-                                                  const BattleShip::AttackResult &attackResult) {
-
-}
-
-void BattleShip::StandardView::showPlayersBoard(const BattleShip::Player &player) {
-
-}
-
-void BattleShip::StandardView::showPlacementBoard(const BattleShip::Player &player) {
+void BattleShip::StandardView::printBoardAsVisible(const Board &board) {
+    std::vector<std::string>  visBoard = board.getVisibleVersion();
+    for(int i = 0; i < board.getNumRows();i++){
+        std::cout<<visBoard[i]<<endl;
+    }
 
 }
 
 void BattleShip::StandardView::printBoard(const std::vector<std::string> &board) {
+    printBoardAsObscured(board);
+    printBoardAsVisible(board);
+}
+
+
+void BattleShip::StandardView::showPlayersBoard(const Player& player){
+    printBoard(player.getBoard());
+}
+
+void BattleShip::StandardView::showPlacementBoard(const Player &player) {
+    printBoardAsVisible(player.getBoard());
+}
+
+void BattleShip::StandardView::updateShipPlacementView(const Player &player) {
+    showPlacementBoard(player);
+}
+
+
+ShipPlacement BattleShip::StandardView::getShipPlacement(const Player &player, char shipChar, int shipLen) {
+    char input;
+    int row;
+    int col;
+
+    int endRow;
+    int endCol;
+
+
+
+    out<<player.getName()<< ",  do you want to place "<<shipChar<<" horizontally or vertically?\n"
+                                                                  "Enter h for horizontal or v for vertical\n"
+                                                                  "Your choice: "<<endl;
+
+    verifiedRead(in, input);
+
+    if(input == 'v'){
+
+
+        out<<player.getName() << ",  enter the row and column you want to place" << shipChar << ", which is " <<shipLen<< " long, at with a space in between row and col:"<<endl;
+        verifiedRead(in, row);
+        verifiedRead(in, col);
+
+        endRow = row+shipLen;
+        endCol = col;
+
+
+    }else
+    if( input == 'h'){
+
+        out<<player.getName() << ",  enter the row and column you want to place" << shipChar << ", which is " <<shipLen<< " long, at with a space in between row and col:"<<endl;
+        verifiedRead(in, row);
+        verifiedRead(in, col);
+
+
+        endCol = row+shipLen;
+        endRow = row;
+
+    }
+
+    BattleShip::ShipPlacement place(row, col, endRow, endCol);
+
+    place.normalize();
+
+    return  place;
 
 }
 
-void BattleShip::StandardView::printBoardAsVisible(const BattleShip::Board &board) {
 
-}
 
-void BattleShip::StandardView::printBoardAsObscured(const BattleShip::Board &board) {
-
-}
-
-int BattleShip::StandardView::getAiChoice() {
-    return 0;
-}
-
-void BattleShip::StandardView::clearInput() {
-
-}
